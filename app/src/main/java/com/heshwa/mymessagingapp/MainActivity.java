70bbc0da7e6dfc92182c127d7fb4e879bfcfa23f
Heshwa <heshwabaskaran@gmail.com>
2020-07-09 10:20:00 +0530

    All users will be displayed in MainActivity

--- app/src/main/java/com/heshwa/mymessagingapp/MainActivity.java
+++ app/src/main/java/com/heshwa/mymessagingapp/MainActivity.java
@@ -1,15 +1,176 @@
 package com.heshwa.mymessagingapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView txtdiplayUserName;
+    private DatabaseReference userRef;
+    private ListView mListView;
+    private ArrayList<String> userArrayList;
+    private static ArrayList<String> userIds;
+    private ArrayAdapter mArrayAdapter;
+    private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = findViewById(R.id.listViewUsers);
+        txtdiplayUserName = findViewById(R.id.txtDisplayName);
+        userArrayList = new ArrayList<>();
+        userIds = new ArrayList<>();
+        mArrayAdapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,userArrayList);
+        mListView.setAdapter(mArrayAdapter);
+        mToolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(mToolbar);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                Intent intent = new Intent(MainActivity.this, MessageActivity.class);
+                intent.putExtra("ReceiverId",userIds.get(position));
+                intent.putExtra("ReceiverName",userArrayList.get(position));
+                startActivity(intent);
 
+
+
+            }
+        });
+
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setTitle("Getting Users");
+        progressDialog.setMessage("please wait");
+        progressDialog.show();
+        mAuth = FirebaseAuth.getInstance();
+        userRef.child(mAuth.getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                txtdiplayUserName.setText(snapshot.child("Name").getValue().toString());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+        userRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if(snapshot.exists() && snapshot!= null && !snapshot.getKey().equals(mAuth.getCurrentUser().getUid()))
+                {
+                    userArrayList.add(snapshot.child("Name").getValue().toString());
+                    userIds.add(snapshot.getKey());
+                }
+                mArrayAdapter.notifyDataSetChanged();
+                progressDialog.dismiss();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_mainactivity,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.itmLogOut)
+        {
+            HashMap<String,Object> map = new HashMap<>();
+            map.put("Offline",ServerValue.TIMESTAMP);
+            userRef.child(mAuth.getCurrentUser().getUid()).child("Status").setValue(map);
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        onDestroy();
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("Online",ServerValue.TIMESTAMP);
+        userRef.child(mAuth.getCurrentUser().getUid()).child("Status").setValue(map);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("Offline",ServerValue.TIMESTAMP);
+        userRef.child(mAuth.getCurrentUser().getUid()).child("Status").setValue(map);
     }
 }
